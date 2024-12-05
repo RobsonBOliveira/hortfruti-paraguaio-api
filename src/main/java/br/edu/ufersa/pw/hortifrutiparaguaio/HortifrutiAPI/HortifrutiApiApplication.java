a5078bcba856ce2b10520efc717033daaaf444c9
robson <breninhodograu599@gmail.com>
2024-12-05 16:55:53 -0300

    initial commit

---
+++ src/main/java/br/edu/ufersa/pw/hortifrutiparaguaio/HortifrutiAPI/HortifrutiApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.pw.hortifrutiparaguaio.HortifrutiAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortifrutiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortifrutiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufersa/pw/hortifrutiparaguaio/HortifrutiAPI/HortifrutiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.pw.hortifrutiparaguaio.HortifrutiAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HortifrutiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

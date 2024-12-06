3b4d3ce724c4b03aedcfb31f8e55280e8831dfb2
robson <breninhodograu599@gmail.com>
2024-12-06 13:26:09 -0300

    add: OrderDTO

--- src/main/java/br/edu/ufersa/pw/hortifrutiparaguaio/HortifrutiAPI/api/dto/OrderDTO.java
+++ src/main/java/br/edu/ufersa/pw/hortifrutiparaguaio/HortifrutiAPI/api/dto/OrderDTO.java
@@ -1,16 +1,13 @@
 package br.edu.ufersa.pw.hortifrutiparaguaio.HortifrutiAPI.api.dto;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ProductDTO {
-    private int idProduct;
-    private int idSalesman;
-    private String name;
-    private String description;
+public class OrderDTO {
+    private int orderId;
+    private int productId;
+    private int quantity;
     private double price;
-
 }

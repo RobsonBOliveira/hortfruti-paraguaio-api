3a719d3b17fb737475f04bf2eae74a4452fc09eb
jocineudo <jocineudo.santos2013@gmail.com>
2024-12-07 15:35:34 -0300

    add: Delete Salesman; Post and Delete in Order

--- src/main/java/br/edu/ufersa/pw/hortifrutiparaguaio/HortifrutiAPI/api/restControllers/OrderController.java
+++ src/main/java/br/edu/ufersa/pw/hortifrutiparaguaio/HortifrutiAPI/api/restControllers/OrderController.java
@@ -4,10 +4,7 @@ package br.edu.ufersa.pw.hortifrutiparaguaio.HortifrutiAPI.api.restControllers;
 import br.edu.ufersa.pw.hortifrutiparaguaio.HortifrutiAPI.domain.entities.order.Order;
 import br.edu.ufersa.pw.hortifrutiparaguaio.HortifrutiAPI.domain.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,5 +20,11 @@ public class OrderController {
 
     @GetMapping("/orders/{orderId}")
     public Optional<Order> getOrderById(@PathVariable Long orderId){return order.findById(orderId);}
+
+    @PostMapping("/orders/post")
+    public Order createOrder(@RequestBody Order orders) {return order.save(orders);}
+
+    @DeleteMapping("/orders/delete/{orderId}")
+    public void deleteOrder(@PathVariable Long orderId){order.deleteById(orderId);}
 
 }

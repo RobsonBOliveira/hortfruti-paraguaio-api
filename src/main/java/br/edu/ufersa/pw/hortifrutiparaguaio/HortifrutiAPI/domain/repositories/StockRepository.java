ccc6a04055820786c474e8f952acb5bfd6606c10
Robson Breno <robson.pinheiro@alunos.ufersa.edu.br>
2024-12-07 22:39:37 -0300

    del: remove old stock and user repositories

--- src/main/java/br/edu/ufersa/pw/hortifrutiparaguaio/HortifrutiAPI/domain/repositories/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ufersa.pw.hortifrutiparaguaio.HortifrutiAPI.domain.repositories;
-
-import br.edu.ufersa.pw.hortifrutiparaguaio.HortifrutiAPI.domain.entities.stock.Stock;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StockRepository extends CrudRepository<Stock, Long> {
-}
